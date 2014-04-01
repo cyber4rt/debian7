757987adbd6d55199bb53fc1dd3c42d973faf60e
Muhammad Yuris Azmi <muhammad.yuris.azmi@gmail.com>
2014-04-01 20:38:48 +0700

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -1,9 +1,15 @@
-------------------
-while : do userlimit.sh 1 sleep 5 done
-------------------
-while : do userlimit.sh 2 sleep 5 done
--------------------
-while : do userexpired.sh 1 sleep 5 done
--------------------
-while : do userexpired.sh 2 sleep 5 done
--------------------
+while :
+  do 
+  ./userlimit.sh 1 
+  sleep 5 
+  done
+while :
+  do
+  ./userlimit.sh 2
+  sleep 5 
+  done
+while :
+  do
+  ./userexpired.sh
+  sleep 36000
+  done
