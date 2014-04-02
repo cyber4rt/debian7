705cc926c868a326ea26c87c67f35fba8780c814
Muhammad Yuris Azmi <muhammad.yuris.azmi@gmail.com>
2014-04-02 11:50:39 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -116,6 +116,7 @@ if [ "$OS" == "x86_64" ]; then
   wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/yurisshOS/debian7/master/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.d/rc.local
 chmod +x /usr/bin/badvpn-udpgw
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 
